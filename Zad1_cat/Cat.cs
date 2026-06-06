b9c4ed0c8bce94febf3f19aa557250769f506286
mark2-jzx90 <maksimzuev300@gmail.com>
2026-06-06 12:06:17 +0500

    Add files via upload

---
+++ Zad1_cat/Cat.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1_cat
+{
+    class Cat
+    {
+        private double weight;
+        private string name;
+        public string Name //св-во для имени
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                bool OnlyLetters = true;
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+                if (OnlyLetters && !string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+
+                else
+                {
+                    Console.WriteLine($"{value} - НЕПРАВИЛЬНОЕ ИМЯ!!");
+                    name = "Кот без имени";
+                }
+                    
+            }
+        }
+
+
+        public double Weight //св-во для веса
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if(value > 0 && value <= 15)
+                {
+                    weight = value;
+                }
+                else if(value > 15)
+                {
+                    Console.WriteLine($"{value}кг. Слишком ТОЛСТЫЙ кот. Кот не может весить больше 15 кг.");
+                    weight = 15.0;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильный вес. Ну не может вес быть отрицательным числом.");
+                    weight = 1.0;
+                }
+            }
+        }
+
+        public Cat (string CatName, double CatWeight) //конструтор для калсса Cat
+        {
+            Name = CatName;
+            Weight = CatWeight;
+        }
+
+        public void Meow() //Метод который позволяет котиков мяукать
+        {
+            Console.WriteLine($"{name} (вес: {weight} кг): МЯУУУУУ!");
+        }
+    }
+
+}
---
+++ Zad1_cat/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1_cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи кол-во котов: ");
+            int catCount;
+            while(!int.TryParse(Console.ReadLine(), out catCount) || catCount <= 0)
+            {
+                Console.WriteLine("Неккорктный ввод, нужно вести положительное число");
+            }
+            Cat[] cats = new Cat[catCount];
+            
+            for(int i = 0; i < catCount; i++)
+            {
+                Console.WriteLine($"Введи имя кота {i + 1}: ");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Введи вес кота {i + 1} (до 15кг)");
+                double weight;
+                while (!double.TryParse(Console.ReadLine(), out weight))
+                {
+                    Console.Write("Ошибка! Введи корректное число!");
+                }
+                cats[i] = new Cat(name, weight);
+            }
+            Console.WriteLine("\nКоты: ");
+            foreach(Cat cat in cats)
+            {
+                cat.Meow();
+            }
+            Console.ReadKey();
+        }
+    }
+}
